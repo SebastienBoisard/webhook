900d335522985ea05d203a691719bdc72b12d87c
SebastienBoisard <sebastien.boisard@gmail.com>
2017-02-15 21:24:10 +0100

    . get header data (token and type of event) from GitLab . check if the token is the right one . compare the event to the possible one

--- main.go
+++ main.go
@@ -6,19 +6,74 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"io"
 )
 
 func handleGitLab(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("handleGitLab")
 	requestDump, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(string(requestDump))
+
+	// Doc: https://docs.gitlab.com/ce/user/project/integrations/webhooks.html
+
+	token := r.Header.Get("X-Gitlab-Token")
+	if len(token) == 0 {
+		fmt.Println("No token!")
+	}
+	fmt.Println("token=", token)
+
+	if token != *gitlabToken {
+		fmt.Println("Wrong token!")
+		w.Header().Set("Content-type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		io.WriteString(w, "{}")
+		return
+	}
+
+	event := r.Header.Get("X-Gitlab-Event")
+	if len(event) == 0 {
+		fmt.Println("No event!")
+		w.Header().Set("Content-type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "{}")
+		return
+	}
+	fmt.Println("event=", event)
+
+	switch event {
+	case "Push Hook":
+		fmt.Println("Push Hook")
+	case "Tag Push Hook":
+		fmt.Println("Tag Push Hook")
+	case "Issue Hook":
+		fmt.Println("Issue Hook")
+	case "Note Hook":
+		fmt.Println("Note Hook")
+	case "Merge Request Hook":
+		fmt.Println("Merge Request Hook")
+	case "Wiki Page Hook":
+		fmt.Println("Wiki Page Hook")
+	case "Pipeline Hook":
+		fmt.Println("Pipeline Hook")
+	case "Build Hook":
+		fmt.Println("Build Hook")
+	default:
+		fmt.Println("unknown event")
+	}
+
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "{}")
+	return
 }
 
-var address = flag.String("addr", "localhost:8080", "http service address")
+var address = flag.String("addr", "192.168.1.26:8080", "http service address")
 
-//var token = flag.String("token", "1234", "token to identify this client")
+var gitlabToken = flag.String("token", "test", "token to verify is the request is legitimate")
 
 func main() {
 
@@ -27,4 +82,5 @@ func main() {
 
 	http.HandleFunc("/gitlab", handleGitLab)
 	log.Fatal(http.ListenAndServe(*address, nil))
+//	log.Fatal(http.ListenAndServeTLS(*address, "server.crt", "server.key", nil))
 }
